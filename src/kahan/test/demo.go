f3348ac0eabede3e3c3543bcfb99149a5eb6bd75
a-shaw <anthony.shaw999@gmail.com>
2018-04-26 23:00:51 -0400

    Initial commit

---
+++ src/kahan/test/demo.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"math"
+
+	"github.com/a-shaw/gona/src/kahan"
+)
+
+const SeriesSize = 100000
+
+func main() {
+	rnums := make([]float64, SeriesSize)
+	perm := rand.Perm(len(rnums)) // generate random numbers
+
+	for i := range rnums {
+		rnums[perm[i]] = float64(i) / 13
+	}
+
+	// fmt.Println(nums)
+
+	n := SumNaive(rnums)
+	k := kahan.SumAll(rnums)
+
+	fmt.Printf("Naive sum: %.10f\n", n)
+	fmt.Printf("Kahan sum: %.10f\n", k)
+
+	diff := math.Abs((n-k))
+	fmt.Printf("Difference: %.10f\n", diff)
+
+}
+
+func SumNaive(nums []float64) float64 {
+	var sum float64
+	for _, num := range nums {
+		sum += num
+	}
+	return sum
+}
